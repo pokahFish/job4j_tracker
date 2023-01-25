ff1122169f34dfb8425d70b132be0ff2540acaf4
pokahFish <pokahfish@gmail.com>
2023-01-25 15:02:53 +0400

    2.0. Рефакторинг - Расстояние между точками. [#122587 #384754]

--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -1,8 +1,7 @@
 package ru.job4j.oop;
 
-import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class PointTest {
 
@@ -10,27 +9,27 @@ public class PointTest {
     public void whenA00B01ThenDistance1() {
         Point a = new Point(0, 0);
         Point b = new Point(0, 1);
-        a.distance(b);
+        double out = a.distance(b);
         double expected = 1.0;
-        assertThat(a.distance(b)).isEqualTo(expected);
+        Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
-    public void whenA13B27ThenDistance4d1() {
+        public void whenA13B27ThenDistance4d1() {
         Point a = new Point(1, 3);
         Point b = new Point(2, 7);
-        a.distance(b);
-        double expected = 4.123105625617661;
-        assertThat(a.distance(b)).isEqualTo(expected);
+        double out = a.distance(b);
+        double expected = 4.12;
+        Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void whenA22B13ThenDistance1d4() {
         Point a = new Point(2, 2);
         Point b = new Point(1, 3);
-        a.distance(b);
-        double expected = 1.4142135623730951;
-        assertThat(a.distance(b)).isEqualTo(expected);
+        double out = a.distance(b);
+        double expected = 1.41;
+        Assert.assertEquals(expected, out, 0.01);
     }
 
 }
